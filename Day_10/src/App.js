526a5e37d3ea06410961cfd3e438f891d6923109
nive-2003 <727821tucs146@gmail.com>
2023-10-06 14:51:01 +0530

    Day_11

---
+++ Day_10/src/App.js
@@ -0,0 +1,30 @@
+// import { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './Login_Register/login';
+import Forgot from './Forgot_password/forgot';
+import Navbar from './Component/Navbar/Navbar';
+import MainPage from './Component/Homepage/MainPage';
+import { ChakraProvider, ColorModeScript, theme } from '@chakra-ui/react';
+import Footer from './Footer/Footer';
+import Privacy from './More/Privacy';
+import FAQ from './More/FAQ';
+import TAC from './More/TAC';
+
+function App() {
+	return (
+   
+        <Router>
+          <Routes>
+          <Route path="/Login" element= {<><Login /></>}/>
+          <Route path="/" element= {<><ChakraProvider> <ColorModeScript initialColorMode={theme.config.initialColorMode}/><Navbar /> <MainPage /><Footer/></ChakraProvider></>}/>
+          <Route path="/forgot" element= {<><Forgot /></>}/>
+          <Route path="/privacy" element= {<><Privacy/></>}/>
+          <Route path="/faq" element= {<><FAQ/></>}/>
+          <Route path="/tac" element= {<><TAC/></>}/>
+          </Routes>
+        </Router>
+        
+  )
+  }
+  
+export default App;
